b1bb710c0b5ebde10d2c8748199f2214a7fcd442
Isale <isale.bryan@gmail.com>
2020-07-30 15:46:22 +0300

    first commit

---
+++ staffregister.php
@@ -0,0 +1,114 @@
+<?php 	require_once('connect.php'); ?>
+<!DOCTYPE html>
+<html>
+  <head>
+    <title>Online Laundry</title>
+    <link rel="stylesheet" type="text/css" href="stylesbackup1.css">
+    <style>
+    .navactive {
+        color: white;
+        background-color: #bcb562;
+    }
+    </style>
+  </head>
+  <div class="header">
+    <div class="logoWrapper">
+      <img class="logo" src="BNN.png" border="0" />
+      <div class="topnav">
+        <a href="staffsignin.php">Log in</a>
+        <a href="staffregister.php">Sign up</a>
+        <a href="management.php">Home</a>
+      </div>
+    </div>
+
+  </div>
+  <body>
+
+    <div class="registerContent">
+      <div class="registerDiv">
+        <h1>Sign Up Here</h1>
+        <hr>
+        <br>
+        <form action="finishregister.php" method="post">
+          
+
+          <label for="fname">First Name</label>
+          <input type="text" id="fname" name="firstName" placeholder="Your name..">
+
+          <label for="lname">Last Name</label>
+          <input type="text" id="lname" name="lastName" placeholder="Your last name..">
+
+          <label for="status">Gender</label>
+          <select class="statusSelect" name="gender">
+            <option value="M">Male</option>
+            <option value="F">Female</option>
+          </select>
+          
+          <label for="citizenNo">Member Number</label>
+          <input type="text" id="citizenNo" name="citizenNumber" placeholder="Ex.1">
+
+          <label for="dob">Date of birth</label>
+          <input type="date" id="dob" name="dateOfBirth" placeholder=""><br>
+
+          <label for="mobileNo">Mobile Number</label>
+          <input type="text" id="mobileNo" name="mobileNumber" placeholder="Ex. 07123453213">
+
+          <label for="address">Address</label>
+          <input type="address" id="address" name="address" placeholder="Nakuru, Shabab">
+
+          <label for="mail">Email</label>
+          <input type="text" id="mail" name="email" placeholder="Your Email">
+
+          <label for="userid">ID for this website</label>
+          <input type="text" id="userid" name="userId" placeholder="Your ID">
+
+          <label for="password">Password</label>
+          <input type="password" id="password" name="password" placeholder="Your password">
+
+          <label for="rePassword">Retype Password</label>
+          <input type="password" id="rePassword" name="rePassword" placeholder="Your password">
+
+
+          <input type="submit" name="staffRegisterSubmit"value="Submit">
+        </form>
+      </div>
+    </div>
+      <div class="regisImageWrapper">
+        <img src="regis.jpg" alt="">
+      </div>
+
+  <footer>
+    <div class="footerContent">
+      <div class="leftFooter">
+        <h1 id="followUs">Follow us . . </h1>
+        <div class="iconWrapper">
+          <a href="https://www.facebook.com/">
+            <img class="socialIcon" src="facebook.png" border="0" />
+          </a>
+          <a href="https://www.instagram.com/">
+            <img class="socialIcon" src="ig.png" border="0" />
+          </a>
+          <a href="https://twitter.com/">
+            <img class="socialIcon" src="twitter.png" border="0" />
+          </a>
+        </div>
+      </div>
+      <div class="rightFooter">
+        <div class="memberCredit">
+          <h1>Created by</h1>
+        </div>
+        <div class="nameList">
+          <ul id="name">
+            <li>Godfrey</li>
+            <li></li>
+            <li></li>
+          </ul>
+
+        </div>
+      </div>
+    </div>
+  </footer>
+
+  </body>
+
+</html>
---
+++ welcome.php
@@ -0,0 +1,81 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <title>Online Cleaning System</title>
+    <link rel="stylesheet" type="text/css" href="stylesbackup1.css">
+
+  </head>
+  <div class="header">
+    <div class="logoWrapper">
+      <img class="logo" src="BNN.png" border="0" />
+      <!-- <div class="topnav">
+        <a href="signin.php">Log in</a>
+        <a href="register.php">Sign up</a>
+        <a href="mainpage.php">Home</a>
+      </div> -->
+    </div>
+    <div class="motto">
+      <h1>We provide professional laundry service</h1>
+    </div>
+  </div>
+  <body>
+    <div class="row">
+    <div class="content">
+
+      <div class="boxWrapper high400">
+        <div class="withBorder">
+          <a href="index.php">
+            
+          </a>
+          <a href="management.php">
+            <div class="welcome Staff">
+              <h2>Login or Register Here</h2>
+            </div>
+          </a>
+      </div>
+      <!-- <div class="guest">
+        <h3>Guest</h3>
+
+      </div> -->
+    </div>
+
+    </div>
+  </div>
+
+  <footer>
+    <div class="footerContent">
+      <div class="logoWrapper">
+        <img class="logo" src="BNN.png" border="0" />
+      </div>
+      <div class="leftFooter">
+        <h1 id="followUs">Follow us . . </h1>
+        <div class="iconWrapper">
+          <a href="https://www.facebook.com/">
+            <img class="socialIcon" src="facebook.png" border="0" />
+          </a>
+          <a href="https://www.instagram.com/">
+            <img class="socialIcon" src="ig.png" border="0" />
+          </a>
+          <a href="https://twitter.com/">
+            <img class="socialIcon" src="twitter.png" border="0" />
+          </a>
+        </div>
+      </div>
+      <div class="rightFooter">
+        <div class="memberCredit">
+          <h1>Created By</h1>
+        </div>
+        <div class="nameList">
+          <ul id="name">
+            <li>Godfrey</li>
+            <li>Kipng'eno</li>
+            <li></li>
+          </ul>
+
+        </div>
+      </div>
+    </div>
+  </footer>
+  </body>
+
+</html>
